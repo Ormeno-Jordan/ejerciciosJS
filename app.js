bbd439b81b91be9d7ae3fe5c1061e64790e3e562
Jordan-Ormeno <rapsolo7777@gmail.com>
2023-05-29 09:55:46 -0300

    avance de ejercicios

---
+++ app.js
@@ -0,0 +1,32 @@
+//ejercicio 1//
+let miNombre
+miNombre = "Jordan"
+console.log(miNombre)
+//ejercicio 2//
+let miApellido
+miApellido = "Ormeño"
+console.log(miApellido)
+//ejercicio 3//
+let miEdad
+miEdad = "33 años"
+console.log(miEdad)
+//ejercicio 4//
+let miMascota
+miMascota = "Gata"
+console.log(miMascota)
+//ejercicio 5//
+let edadMascota
+edadMascota = "4 años"
+//ejercicio 6//
+
+//ejercicio 7//
+let nombreCompleto
+nombreCompleto = miNombre + " " + miApellido
+console.log(nombreCompleto)
+//ejercicio 8//
+
+//ejercicio 9//
+
+//ejercicio 10//
+
+
